5dc789ea0d15e09fb820ab64b903640511938833
Yaroslav Kravchenko <yaroslavkravch@gmail.com>
2022-01-23 01:24:43 +0200

    Add counter

--- src/modules/game-page/components/UserHand.js
+++ src/modules/game-page/components/UserHand.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
 import UserCardItem from "./UserCardItem";
 
 const UserHand = ({ userHand }) => {
   let userCards = userHand.map((card) => (
     <UserCardItem key={card.code} image={card.image} code={card.code} />
   ));
+
+  const valueArray = userHand.map((card) => {
+    if (card.value === 11) {
+      card.value = 2;
+    } else if (card.value === 12) {
+      card.value = 3;
+    } else if (card.value === 13) {
+      card.value = 4;
+    } else if (card.value === 14) {
+      card.value = 1;
+    }
+    return card;
+  });
+
+  let points = 0;
+  for (let i = 0; i < valueArray.length; i++) {
+    points += +valueArray[i].value;
+  }
+
   return (
     <div className="user">
+      <h2>You have: {points}</h2>
       <div className="user-hand">{userCards}</div>
     </div>
   );
