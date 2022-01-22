83cb5c239073891bb4bf545abc8c14277828be65
Yaroslav Kravchenko <yaroslavkravch@gmail.com>
2022-01-22 17:59:44 +0200

    Add sorts

--- src/modules/deck-page/actions/deckActions.js
+++ src/modules/deck-page/actions/deckActions.js
@@ -46,3 +46,31 @@ export const receiveDeck = () => async (dispatch) => {
     dispatch(failure());
   }
 };
+
+export const receivePartDeck = (suit) => async (dispatch) => {
+  dispatch(request());
+  try {
+    const { data: receiveDeckData } = await getDeckAPI();
+    const { cards } = receiveDeckData;
+    const sortedCards = cards
+      .map((card) => {
+        if (card.value === "JACK") {
+          card.value = 11;
+        } else if (card.value === "QUEEN") {
+          card.value = 12;
+        } else if (card.value === "KING") {
+          card.value = 13;
+        } else if (card.value === "ACE") {
+          card.value = 14;
+        }
+        return card;
+      })
+      .sort((a, b) => +a.value - +b.value)
+      .filter((card) => card.suit === suit);
+
+    dispatch(success(sortedCards));
+  } catch (e) {
+    // TODO - handle this errors
+    dispatch(failure());
+  }
+};
--- src/modules/deck-page/components/ControllPanel.js
+++ src/modules/deck-page/components/ControllPanel.js
@@ -1,6 +1,13 @@
 import { useDispatch } from "react-redux";
-import { receiveDeck } from "./../actions/deckActions";
-import { shuffle } from "./../actions/deckActions";
+import {
+  receiveDeck,
+  shuffle,
+  receivePartDeck,
+} from "./../actions/deckActions";
+import heart from "../../../assets/images/hearts.png";
+import clubs from "../../../assets/images/clubs.png";
+import diamonds from "../../../assets/images/diamonds.png";
+import spade from "../../../assets/images/spade.png";
 
 const ControllPanel = () => {
   const dispatch = useDispatch();
@@ -15,6 +22,32 @@ const ControllPanel = () => {
       >
         Shuffle
       </button>
+      <div className="controll-panel-sort">
+        <div
+          onClick={() => dispatch(receivePartDeck("HEARTS"))}
+          className="controll-panel-sort-item"
+        >
+          <img src={heart} alt="heart" />
+        </div>
+        <div
+          onClick={() => dispatch(receivePartDeck("CLUBS"))}
+          className="controll-panel-sort-item"
+        >
+          <img src={clubs} alt="clubs" />
+        </div>
+        <div
+          onClick={() => dispatch(receivePartDeck("DIAMONDS"))}
+          className="controll-panel-sort-item"
+        >
+          <img src={diamonds} alt="diamonds" />
+        </div>
+        <div
+          onClick={() => dispatch(receivePartDeck("SPADES"))}
+          className="controll-panel-sort-item"
+        >
+          <img src={spade} alt="spade" />
+        </div>
+      </div>
     </div>
   );
 };
